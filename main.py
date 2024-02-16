e2e6a7eb629d403e1bdf8fd6a052e1537f885099
prakarsh17 <pranjal.prakarsh@outlook.com>
2024-02-16 10:11:05 +0530

    Major changes (more than I can list)

--- main.py
+++ main.py
@@ -17,7 +17,15 @@ import json
 from plyer import notification
 from urllib.parse import quote
 import keyboard
+import threading
+from geopy.geocoders import Nominatim  # importing geopy library
+import pywhatkit
+import skills.openapplications as openapplications
+import skills.findfiles as findfiles
+import skills.weather as weather
 
+# calling the Nominatim tool
+loc = Nominatim(user_agent="GetLoc")
 # import hugginfaceblenderbot
 
 engine = pyttsx3.init("sapi5")
@@ -25,23 +33,36 @@ voices = engine.getProperty("voices")
 print(voices)
 engine.setProperty("voice", voices[1].id)  # Choose female voice
 engine.setProperty("volume", 1)
-engine.setProperty("rate", 150)
+engine.setProperty("rate", 190)
 
 assistant_name = "Aira"  # Customize assistant name
+import pygame
+
+# Initialize pygame mixer
+pygame.mixer.init()
+
+# ...
+
+
+def play_music(music_dir):
+    songs = os.listdir(music_dir)
+    for x in songs:
+        speak(f"Now playing... {x}")
+        pygame.mixer.music.load(os.path.join(music_dir, x))
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)  # Adjust the tick value as needed
 
 
 def notify(t, m):
     notification.notify(
-        title=t,
-        message=m,
-        app_icon=None,
-        timeout=5,
+        title=t, message=m, app_icon=None, timeout=5,
     )
 
 
 def mode_select():
     speak(f"Please select an input mode for interacting with {assistant_name}")
-    speak("1.Press 't' for Text Input Mode\n2.Press 's' for Speech Input Mode ")
+    speak("Press 't' for Text Input Mode\nPress 's' for Speech Input Mode ")
 
     while True:
         if keyboard.is_pressed("t"):
@@ -129,30 +150,6 @@ def speak_definition(word, definition_data):
         speak(f"Antonyms for {word} include: {', '.join(antonyms)}.")
 
 
-def hugginfacechat():
-    print("The bot is ready to talk!! (Type 'quit' to exit)")
-    tokenizer, model = hugginfacechat.load_tokenizer_and_model()
-    chat_round = 1
-    chat_history_ids = None
-    while True:
-        inp = input("\nYou: ")
-
-        if inp.lower() == "quit":
-            break
-        elif inp.lower() == "let's start":
-            speak("Great! Let's start the chat.")
-            chat_round = 1
-            chat_history_ids = None
-        elif inp.lower() == "exit":
-            speak("Ending the chat. Goodbye!")
-            break
-        else:
-            chat_history_ids = hugginfacechat.generate_response(
-                tokenizer, model, chat_round, chat_history_ids
-            )
-            chat_round += 1
-
-
 def get_news(category):
     try:
         newsapi = os.environ.get("NEWSAPI")
@@ -189,8 +186,56 @@ def get_user_preference():
     speak(
         "Sure! What type of news would you like to hear? (world/national/something(enter word))"
     )
-    user_preference = takeCommand().lower()
+    user_preference = takeCommand(mode).lower()
     return user_preference
+
+
+def get_fun_fact():
+    api_url = "https://uselessfacts.jsph.pl/random.json?language=en"
+    response = requests.get(api_url)
+    data = response.json()
+    fun_fact = data["text"]
+    speak(f"Here's a fun fact for you: {fun_fact}")
+
+
+# Search for programming tutorials on YouTube
+def search_programming_tutorials():
+    speak("What programming topic are you interested in?")
+    topic = takeCommand(mode)
+    webbrowser.open(
+        f"https://www.youtube.com/results?search_query={quote(topic)}+programming+tutorial"
+    )
+    speak(f"I found some programming tutorials on YouTube for {topic}. Check them out!")
+
+
+def get_tech_news():
+    api_key = os.getenv("NEWS_API_KEY")
+    url = f"https://newsapi.org/v2/top-headlines?category=technology&apiKey={api_key}"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["status"] == "ok" and data["articles"]:
+        articles = data["articles"][:5]
+        speak("Here are some top technology news headlines:")
+        for i, article in enumerate(articles, start=1):
+            speak(f"{i}. {article['title']}")
+            speak(article["description"])
+    else:
+        speak("Sorry, I couldn't fetch technology news at the moment.")
+
+
+def send_whatsapp_message():
+    speak("Whom do you want to send a WhatsApp message?")
+    contact = takeCommand()
+    speak(f"What message would you like to send to {contact}?")
+    message = takeCommand()
+    speak("Sending the message.")
+    pywhatkit.sendwhatmsg(
+        contact,
+        message,
+        datetime.datetime.now().hour,
+        datetime.datetime.now().minute + 1,
+    )
 
 
 def get_joke():
@@ -198,6 +243,7 @@ def get_joke():
         api_url = f"https://api.popcat.xyz/joke"
         response = requests.get(api_url)
         data = response.json()
+        print(data)
         joke = response["joke"]
     except Exception as e:
         print(e)
@@ -205,24 +251,29 @@ def get_joke():
     return joke
 
 
-def takeCommand():
-    """Capture and return a user command, allowing voice and text input."""
-    r = sr.Recognizer()
-
-    try:
-        # Listen for voice input
+def takeCommand(mode):
+    if mode == 1:
+        """Capture and return a user command, allowing voice and text input."""
+        r = sr.Recognizer()
 
         print("Speak your wish...")
         with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source)
             r.pause_threshold = 1
             r.dynamic_energy_threshold = False
-            audio = r.listen(source, timeout=15)  # Set timeout to 30 seconds
+            r.adjust_for_ambient_noise(source, duration=2)
+
+            audio = r.listen(source)  # Set timeout to 30 seconds
         print("Recognizing voice input...")
-        query = r.recognize_google(audio, language="en-in")
+        try:
+            query = r.recognize_google(audio, language="en-in")
+        except Exception:
+            pass  # gotta retry
         print(f"User > {query}\n")
 
         return query
-    except Exception as e:
+
+    elif mode == 0:
         # print(e)
 
         print("Type your command:")
@@ -230,7 +281,7 @@ def takeCommand():
         # Allow text input as backup
 
         query = input("> ")
-        return query
+    return query
 
 
 def sendEmail(to, password, content):
@@ -255,11 +306,11 @@ if __name__ == "__main__":
     # command before the execution of this python file
 
     clear()
-
+    mode = mode_select()
     mystical_greet()
 
     while True:
-        query = takeCommand().lower()
+        query = takeCommand(mode).lower()
 
         if "wikipedia" in query:
             speak("Searching Wikipedia...")
@@ -267,18 +318,23 @@ if __name__ == "__main__":
             try:
                 results = wikipedia.summary(query, sentences=5)
             except wikipedia.DisambiguationError as e:
+                engine.setProperty("rate", 500)
                 speak("There are multiple options. Please specify.")
-                for i, option in enumerate(e.options, start=1):
+                for i, option in enumerate(e.options[:10], start=1):
                     speak(f"{i}. {option}")
                 choice = int(input("Enter the number of your choice: "))
+                engine.setProperty("rate", 200)
                 results = wikipedia.summary(e.options[choice - 1], sentences=5)
             except Exception as error:
-                pass
+                print(error)
+
             speak("According to Wikipedia")
-            speak(results)
+            if results:
+                speak(results)
+            else:
+                speak("No results were there")
         elif "open youtube" in query:
-            speak("Here you go to Youtube")
-            webbrowser.open("https://www.youtube.com")
+            speak(openapplications.Open_Applications.youtube())
         elif "youtube" in query:
             search_query = query.replace("youtube", "").strip()
             url_search = quote(search_query)
@@ -286,17 +342,21 @@ if __name__ == "__main__":
                 f"https://www.youtube.com/results?search_query={url_search}"
             )
         elif "open google" in query:
-            speak("Here you go to Google\n")
-            webbrowser.open("google.com")
+            speak(openapplications.Open_Applications.google())
+        elif "search on google" in query:
+            search_query = query.replace("search on google", "")
+            url_search = quote(search_query)
+            webbrowser.open("https://www.google.com/search?q=" + "+".join(url_search))
+            speak("Opening your browser for desired results")
         elif "open stackoverflow" in query:
-            speak("Here you go to Stack Over flow.Happy coding")
+            speak(openapplications.Open_Applications.stackoverflow())
             webbrowser.open("stackoverflow.com")
         elif "open aisc" in query:
-            speak("Here you go to AI student Community")
+            speak(openapplications.Open_Applications.aisc())
             webbrowser.open("aistudent.community")
         elif "open forum" in query or "open aisc forum" in query:
-            speak("Here you go to AISC Forum")
-            webbrowser.open("forum.aistudent.community")
+            speak(openapplications.Open_Applications.aisc_forum())
+
         elif "define" in query:
             query = query.replace("define", "")
             try:
@@ -307,17 +367,21 @@ if __name__ == "__main__":
         elif "play music" in query or "play song" in query:
             speak("Here you go with music")
 
-            music_dir = "something"
-            songs = os.listdir(music_dir)
-            print(songs)
-            random_song = os.startfile(os.path.join(music_dir, random.choice(songs)))
+            music_dir = r"C:\Users\ADMIN\Desktop\Pranjal Prakarsh\[M] Media\Tunes"
+
+            # Create a new thread for playing music
+            music_thread = threading.Thread(target=play_music, args=(music_dir,))
+            music_thread.start()
+
         elif "the time" in query:
+
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"The time is {strTime}")
+
         elif "send a mail" in query:
             try:
                 speak("What message I should deliver through the ether?")
-                content = takeCommand()
+                content = takeCommand(mode)
                 speak("Whom should I deliver the magical letter? ")
                 to = input("Enter recipient's address > ")
                 password = input("Enter your email password > ")
@@ -326,6 +390,7 @@ if __name__ == "__main__":
             except Exception as e:
                 print(e)
                 speak("I am not able to send this email")
+
         elif "how are you" in query:
             speak("I am fine, Thank you")
             speak("How are you ?")
@@ -333,11 +398,11 @@ if __name__ == "__main__":
             speak("It's good to know that you're fine")
         elif "change name" in query:
             speak("What would you like to call me, ")
-            assistant_name = takeCommand()
+            assistant_name = takeCommand(mode)
             speak("Thanks for naming me")
         elif "change volume" in query or "set volume" in query:
             speak("Please tell the volume level (0.0 to 1.0)")
-            volume = float(takeCommand())
+            volume = float(takeCommand(mode))
             try:
                 engine.setProperty(volume=volume)
                 speak("Volume set to the desired level")
@@ -346,6 +411,8 @@ if __name__ == "__main__":
                     "There was some problem in setting that volume, please recheck values"
                 )
                 pass
+        elif "change mode" in query or "mode" in query:
+            mode_select()
         elif "what's your name" in query or "what is your name" in query:
             speak("My friends call me")
             speak(assistant_name)
@@ -392,20 +459,43 @@ if __name__ == "__main__":
             speak(
                 f"For how much time you want to stop {assistant_name} from listening commands (in seconds)?"
             )
-            a = int(takeCommand())
-            time.sleep(a)
+            a = int(takeCommand(mode))
+            speak("You can also press 'w' key to resume")
+
+            # Add a loop to check if 'w' key is pressed during the sleep
+            for _ in range(a * 10):  # Assuming you check every 0.1 seconds
+                if keyboard.is_pressed("w"):
+                    speak("Resuming...")
+                    break
+
+                time.sleep(0.1)
+
             speak("Master, the wait is over...")
         elif "where is" in query:
+
+            time.sleep(3)
             query = query.replace("where is", "")
             location = query
             speak("User asked to Locate")
             speak(location)
-            webbrowser.open("https://www.google.nl / maps / place/" + location + "")
+            try:
+                getLoc = loc.geocode(location)
+            except:
+                ind = query.split().index("is")
+                location = query[ind + 8 :]
+                url = "https://www.google.com/maps/place/" + "".join(location)
+                speak("This is where i found, " + str(location))
+                webbrowser.open(url)
+                pass
+            if getLoc:
+                speak(getLoc.address)
+                webbrowser.open(getLoc.address)
+
         elif "restart" in query:
             subprocess.call(["shutdown", "/r"])
-        elif "hibernate" in query or "sleep" in query:
+        elif "hibernate" in query:
             speak("Hibernating")
-            subprocess.call("shutdown / h")
+            subprocess.call("shutdown /h")
         elif "log off" in query or "sign out" in query:
             speak("Make sure all the applications are closed before sign-out")
             time.sleep(5)
@@ -432,12 +522,44 @@ if __name__ == "__main__":
             speak("Model - Aira 1 Version 0 at your service")
             speak(assistant_name)
         elif "weather" in query:
-            api_key = "Api key"
-            base_url = "http://api.openweathermap.org / data / 2.5 / weather?"
-            speak(" City name ")
-            print("City name : ")
-            city_name = takeCommand()
-            complete_url = base_url + "appid =" + api_key + "&q =" + city_name
-            pass
+            api_key = os.environ.get("WEATHER")
+            print(api_key)  # Replace with your OpenWeatherMap API key
+            speak("Please tell me the city name.")
+            city_name = takeCommand(mode)
+            response = weather.get_weather(api_key, city_name)
+            speak(response)
+        elif "open gmail" in query:
+            webbrowser.open("gmail.com")
+            speak("Google Mail opened")
+
+        elif "take snapshot" in query or "screen snip" in query:
+            speak("Opening screen snipping tool")
+            keyboard.press_and_release("win+shift+s")
+        elif "dictate" in query:
+            speak("Opening dictation mode")
+        elif "fun fact" in query:
+            get_fun_fact()
+            keyboard.press_and_release("win+H")
+        elif "image conversion" in query:
+            ima = input("Enter the Path of Image :")
+            pywhatkit.image_to_ascii_art(
+                img_path=ima, output_file=f"{assistant_name}_asciiart"
+            )
+            speak("Made your ASCII code and saved it.")
+        elif "find a file" in query or "find file " in query:
+            query = query.replace("find a file", " ")
+            results = findfiles.find_file_in_all_drives(query)
+            for x in results:
+                speak(x)
+
         else:
-            speak("Sorry I don't understand that yet")
+            text = quote(query)
+            botname = quote(assistant_name)
+            url = f"https://api.popcat.xyz/chatbot?msg={text}&owner=Pranjal+Prakarsh&botname={botname}"
+            try:
+                request = requests.get(url)
+                data = request.json()
+                output = data["response"]
+                speak(output)
+            except:
+                speak("Sorry, I don't understand that")
--- skills/findfiles.py
+++ skills/findfiles.py
@@ -1,9 +1,11 @@
 import os
 import re
 import win32api
+
+
 def find_file(root_folder, rex):
-    results=[]
-    for root,dirs,files in os.walk(root_folder):
+    results = []
+    for root, dirs, files in os.walk(root_folder):
         for f in files:
             result = rex.search(f)
             if result:
@@ -11,8 +13,10 @@ def find_file(root_folder, rex):
                 results.append(os.path.join(root, f))
                 return results
 
+
 def find_file_in_all_drives(file_name):
-    #create a regular expression for the file
+    # create a regular expression for the file
     rex = re.compile(file_name)
-    for drive in win32api.GetLogicalDriveStrings().split('\000')[:-1]:
-        find_file( drive, rex )
+    for drive in win32api.GetLogicalDriveStrings().split("\000")[:-1]:
+        print(drive)
+        find_file(drive, rex)
