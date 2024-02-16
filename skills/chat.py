456ef450ed39b8287464ea9877b676adb5b9afe3
prakarsh17 <pranjal.prakarsh@outlook.com>
2024-02-16 18:16:08 +0530

    New chat cmd

--- skills/chat.py
+++ skills/chat.py
@@ -1,30 +1,34 @@
 import requests
 import os
 def get_chat_response(msg_content):
-    url = 'https://api.pawan.krd/v1/chat/completions'
-    api=os.environ.get("OPENAI_API")
-    headers = {
-        'Authorization': 'Bearer ',
-        'Content-Type': 'application/json'
-    }
+    try:
+        url = 'https://api.pawan.krd/v1/chat/completions'
+        api=os.environ.get("OPENAI_API")
+        headers = {
+            'Authorization': 'Bearer {api}',
+            'Content-Type': 'application/json'
+        }
 
-    data = {
-        'model': 'gpt-3.5-turbo',
-        'max_tokens': 100,
-        'messages': [
-            {
-                'role': 'system',
-                'content': 'You are an helpful assistant.'
-            },
-            {
-                'role': 'user',
-                'content': msg_content
-            }
-        ]
-    }
+        data = {
+            'model': 'gpt-3.5-turbo',
+            'max_tokens': 100,
+            'messages': [
+                {
+                    'role': 'system',
+                    'content': 'You are an helpful assistant.'
+                },
+                {
+                    'role': 'user',
+                    'content': msg_content
+                }
+            ]
+        }
 
-    response = requests.post(url, headers=headers, json=data)
-    
-    # Return the response text
-    return response.text
-
+        response = requests.post(url, headers=headers, json=data)
+        data=response.json()
+        print(data)
+        # Return the response text
+        return data
+    except Exception as e:
+        print(e)
+        return None
