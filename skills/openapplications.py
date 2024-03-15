bef40d1d647db822f7151e933b68749627f751c4
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-03-15 16:03:58 +0530

    Format files

--- skills/openapplications.py
+++ skills/openapplications.py
@@ -3,6 +3,7 @@ from plyer import notification
 import keyboard
 from urllib.parse import quote
 
+
 class OpenApplications:
     @staticmethod
     def open_website(url, display_name):
@@ -37,13 +38,13 @@ class OpenApplications:
     @staticmethod
     def open_custom_application(application_name, url):
         return OpenApplications.open_website(url, application_name)
+
     @staticmethod
     def search_yt(query):
         url_search = quote(query)
-        webbrowser.open(
-            f"https://www.youtube.com/results?search_query={url_search}"
-        )
+        webbrowser.open(f"https://www.youtube.com/results?search_query={url_search}")
         return True
+
     @staticmethod
     def search_google(query):
         url_search = quote(query)
